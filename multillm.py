13d25f66067cce6f311af1f959159ab46159206d
samy.abou-al-tout <samy.abou-al-tout@epita.fr>
2025-05-12 04:52:21 +0200

    Response from 2 LLMs with validator - very basic system

---
+++ multillm.py
@@ -0,0 +1,51 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+import gradio as gr
+import os
+
+load_dotenv(override=True)
+
+openai_api_key = os.getenv('OPENAI_API_KEY')
+google_api_key = os.getenv('GEMINI_API_KEY')
+deepseek_api_key = os.getenv('DEEPSEEK_API_KEY')
+
+system_prompt = "You are a helpful medical assistant, you'll give precise an answer but easy and very short" \
+" to understand so that any patient could understand the answer"
+
+
+gemini = OpenAI(
+    api_key=google_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+deepseek = OpenAI(
+    api_key=deepseek_api_key, 
+    base_url="https://api.deepseek.com/v1"
+)
+
+openai = OpenAI()
+
+
+def chat(message, history):
+    messages = [{"role" : "system", "content": system_prompt}] + history + [{"role" : "user", "content": message}]
+    response_1 = gemini.chat.completions.create(
+        model="gemini-2.0-flash",
+        messages=messages
+    )
+    got_1 = response_1.choices[0].message.content
+    response_2 = deepseek.chat.completions.create(
+        model="deepseek-chat",
+        messages=messages
+    )
+    got_2 = response_2.choices[0].message.content
+    judge = [{"role": "system", "content" : "You will have to choose the best answer between the two given and return the strictly better one"},
+             {"role": "user", "content" : f"""You need to choose the best answer between these two answers, 
+              do only return the strictly better one and do not say anything else.
+              \nHere are the two answers: \n Here is the first one: \n {got_1} and here is the second one: \n {got_2} """}]
+    best_response = openai.chat.completions.create(
+        model='gpt-4o-mini',
+        messages=judge
+    )
+    return best_response.choices[0].message.content
+
+gr.ChatInterface(chat, type="messages").launch()
